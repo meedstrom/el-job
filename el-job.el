ecc8e5567847b5f56445b60b8f6877644bfe235e
Martin Edström <meedstrom91@gmail.com>
2025-03-05 00:51:12 +0100

    Release version 1.1.0

--- el-job.el
+++ el-job.el
@@ -19,7 +19,7 @@
 ;; URL:              https://github.com/meedstrom/el-job
 ;; Created:          2024-10-30
 ;; Keywords:         processes
-;; Package-Version:  1.0.5
+;; Package-Version:  1.1.0
 ;; Package-Requires: ((emacs "28.1") (compat "30"))
 
 ;;; Commentary:
