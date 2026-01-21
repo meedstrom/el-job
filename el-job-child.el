979e6940aae89534a8ac43f955b238a0ec80db65
Martin Edström <meedstrom@runbox.eu>
2026-01-21 19:03:43 +0100

    Bump copyright year

--- el-job-child.el
+++ el-job-child.el
@@ -1,6 +1,6 @@
 ;;; el-job-child.el --- Worker code for children  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024-2025 Free Software Foundation, Inc.
+;; Copyright (C) 2024-2026 Free Software Foundation, Inc.
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
